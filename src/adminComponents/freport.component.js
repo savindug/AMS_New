794396b5da3d7ea38985d9f8b181389dca8e0225
Nuwanga <thnuwanga@gmail.com>
2020-06-07 00:23:39 +0530

    admin added

---
+++ src/adminComponents/freport.component.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+
+/*-------------ADMIN------------------------------------------------*/
+/*-------------ADMIN------------------------------------------------*/
+/*-------------ADMIN------------------------------------------------*/
+/*-------------ADMIN------------------------------------------------*/
+class FreportComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>FieldReports Here</h1>
+                <div className="text-center">
+                <button className="btn btn-success"><b>Upload Report</b></button>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default FreportComponent;
